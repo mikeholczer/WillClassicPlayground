189bd81c074ad3fd809762e0ea2b87a5c1e914db
Mike Holczer <github@holczer.com>
2013-07-07 00:26:11 -0400

    control of two motors using wii classic joysticks

--- WiiPlayground/WiiPlayground.ino
+++ WiiPlayground/WiiPlayground.ino
@@ -3,11 +3,23 @@
 
 WiiClassic wiiClassy = WiiClassic();
 
+int m3dir = 2;
+int m3pwm = 3;
+int m4dir = 4;
+int m4pwm = 5;
+
 void setup() {
   Wire.begin();                                                           // Begin wire connection
   Serial.begin(9600);                                                     // Begin Serial on 9600 baud
   wiiClassy.begin();                                                      // Initialize the Wii Classic controller
   wiiClassy.update();                                                     // Gather data from the controller for the first time as this isually bogus
+  
+  pinMode(m3dir, OUTPUT);
+  pinMode(m3pwm, OUTPUT);
+  pinMode(m4dir, OUTPUT);
+  pinMode(m4pwm, OUTPUT);
+  
+  Serial.println("Setup complete");
 }
 
 void loop() {
@@ -118,7 +130,7 @@ void loop() {
   
   Serial.print("DU:");
   if(wiiClassy.upDPressed()){
-    Serial.print("1");
+    Serial.print("1");    
   } 
   else {
     Serial.print("0");
@@ -128,7 +140,7 @@ void loop() {
   Serial.print("DD:");
   if(wiiClassy.downDPressed()){
     Serial.print("1");
-  } 
+      } 
   else {
     Serial.print("0");
   }
@@ -165,6 +177,51 @@ void loop() {
   Serial.print(" ");
 
   Serial.println();
+  
+  int rightPower = 0;
+  int leftPower = 0;
+  int rightDirection = HIGH;
+  int leftDirection = HIGH;
+  
+  int rightY = wiiClassy.rightStickY();
+  if(rightY > 16)
+  {
+    rightDirection = HIGH;
+    rightPower = map(rightY, 17, 28, 1, 250);
+  }
+  if(rightY < 14)
+  {
+    rightDirection = LOW;
+    rightPower = map(rightY, 13, 2, 1, 250);
+  }
+  
+  int leftY = wiiClassy.leftStickY();
+  if(leftY > 33)
+  {
+    leftDirection = HIGH;
+    leftPower = map(leftY, 34, 57, 1, 250);
+  }
+  if(leftY < 31)
+  {
+    leftDirection = LOW;
+    leftPower = map(leftY, 30, 6, 1, 250);
+  }
+  
+  Serial.print("direction:");
+  Serial.print(rightDirection);
+  
+  Serial.print(" ");
+  
+  Serial.print("rightPower: ");
+  Serial.print(rightPower);
+  Serial.println();
+  
+  digitalWrite(m4dir, leftDirection);
+  digitalWrite(m3dir, rightDirection);
+  
+  analogWrite(m4pwm, leftPower);
+  analogWrite(m3pwm, rightPower);
+  
   delay(100);
 }
 
